85e09335aa0b55a206acb08fbf841f0a697cd369
Nasim 👸🏻 <nasim@MacBook-Air-4.local>
2023-10-15 13:46:45 -0400

    Add get user authentication with passprt library

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 //because we used index.ts (barell export pattern.)
 import { AuthDto } from './dto';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('sign-up')
-  //you should not use request staright in here
+  //you should not use request straight in here
   //we need to have DTO (and obj) to transdfer data in it.
   signUp(@Body() dto: AuthDto) {
     return this.authService.signUp(dto);
   }
+  @HttpCode(HttpStatus.OK)
   @Post('sign-in')
-  signIn(dto: AuthDto) {
+  signIn(@Body() dto: AuthDto) {
     return this.authService.signIn(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,8 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
@@ -15,28 +18,41 @@ export class AuthService {
   ) {}
   async signIn(dto: AuthDto) {
     //find the user by email
-    const userFound = await this.prisma.user.findUnique({
-      where: {
-        email: dto.email,
-      },
-    });
+    console.log('this iskjjffkkd', dto.email);
+    const userFound =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
 
+    console.log('userFound', userFound);
     //if user doenst exist throw error
     if (!userFound) {
-      throw new ForbiddenException('Credentials not valid');
+      throw new ForbiddenException(
+        'Credentials not valid',
+      );
     }
 
     //compare passwords
-    const hashCompare = await argon.verify(userFound.hash, dto.password);
+    const hashCompare = await argon.verify(
+      userFound.hash,
+      dto.password,
+    );
 
     //if password incorrect throw error
     if (!hashCompare) {
-      throw new ForbiddenException('Credentials not valid');
+      throw new ForbiddenException(
+        'Credentials not valid',
+      );
     }
-    delete (await userFound).hash;
+    // delete (await userFound).hash; // we dont really need to do this
 
     //send back user
-    return this.signToken(userFound.id, userFound.email);
+    return this.signToken(
+      userFound.id,
+      userFound.email,
+    );
   }
 
   async signUp(dto: AuthDto) {
@@ -45,36 +61,55 @@ export class AuthService {
 
     try {
       //save the user
-      const user = this.prisma.user.create({
+      const user = await this.prisma.user.create({
         data: {
           email: dto.email,
           hash: hash,
         },
       });
-      delete (await user).hash;
+      // delete (await user).hash;
       //if fo instance we do not wanna pass the password we can use select and add those we want to use OR
       // select: {
       //   id: true,
       //   email: true,
       // },
       //return the user
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
-      if (error instanceof PrismaClientKnownRequestError) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
         if (error.code === 'P2002') {
-          throw new ForbiddenException('Credentials taken');
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
         }
       }
       throw error;
     }
   }
 
-  async signToken(userId: number, email: string): Promise<string> {
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
     const payload = {
       sub: userId,
       email,
     };
     const secret = this.config.get('JWT_SECRET');
-    return this.jwt.signAsync(payload, { expiresIn: '15m', secret: secret });
+    const token = await this.jwt.signAsync(
+      payload,
+      {
+        expiresIn: '15m',
+        secret: secret,
+      },
+    );
+
+    return {
+      access_token: token,
+    };
+    // return this.jwt.signAsync(payload, { expiresIn: '15m', secret: secret });
   }
 }
