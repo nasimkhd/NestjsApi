b005c3a3a8a73ba6aefe0ddf5e846ef99973594e
Nasim 👸🏻 <nasim@MacBook-Air-4.local>
2023-10-16 21:09:45 -0400

    modify package js

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import {
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
 
 describe('app e2e', () => {
   let app: INestApplication;
   beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    const moduleRef =
+      await Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(
       new ValidationPipe({
+        //otherwise the validation wouldnt work
         whitelist: true,
       }),
     ); //this is for validation
